9a0203f1989ed1d092f0406442178551c06940dc
Bruno Mendoza <hola@brunomendoza.es>
2021-10-28 19:03:32 +0100

    Building a simple GraphQL API server

--- server.js
+++ server.js
@@ -1,4 +1,24 @@
 const express = require("express")
-const app = express()
+const { graphqlHTTP } = require("express-graphql") 
+const { buildSchema } = require("graphql")
 
-app.listen(5000, () => console.log("Server running"))
+const schema = buildSchema(`
+  type Query {
+      hello: String
+  }
+`)
+
+const root = {
+  hello: () => {
+      return 'Hello world!'
+  }
+}
+
+const app = express()
+// https://expressjs.com/en/guide/using-middleware.html
+app.use("/graphql", graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}))
+app.listen(5000, () => console.log('Running a GraphQL API server at http://localhost:5000/graphql'))
