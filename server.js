2c783607e95340f5beb80b7de0398e4622df2c3c
Bruno Mendoza <hola@brunomendoza.es>
2021-10-31 20:56:00 +0000

    Testing connection to local CouchDB database

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express")
 const { graphqlHTTP } = require("express-graphql") 
 const schema = require('./schema/schema')
 
+const nano = require('nano')('http://admin:admin@db:5984')
+
+nano.db.get('graphql')
+  .then(data => console.log(data))
+  .catch(err => console.err(err))
+
 const app = express()
 // https://expressjs.com/en/guide/using-middleware.html
 app.use("/graphql", graphqlHTTP({
