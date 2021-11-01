03656893f2139ab995801fd64bacc5d2a14c5c58
Bruno Mendoza <hola@brunomendoza.es>
2021-11-01 18:49:13 +0000

    Adding mutations to GraphQL schema

--- schema/schema.js
+++ schema/schema.js
@@ -6,39 +6,7 @@ const {
   GraphQLList
 } = require('graphql')
 
-const owners = [
-  {
-    id: "1",
-    firstName: "John",
-    lastName: "Doe",
-  },
-  {
-    id: "2",
-    firstName: "Jane",
-    lastName: "Doe",
-  },
-]
-
-const pets = [
-  {
-    id: "1",
-    name: "Tobby",
-    genre: "male",
-    ownerId: "1"
-  },
-  {
-    id: "2",
-    name: "Jane",
-    genre: "female",
-    ownerId: "2"
-  },
-  {
-    id: "2",
-    name: "Hercules",
-    genre: "male",
-    ownerId: "1"
-  },
-]
+const nano = require('nano')('http://admin:admin@db:5984')
 
 const PetType = new GraphQLObjectType({
   name: 'Pet',
@@ -48,8 +16,8 @@ const PetType = new GraphQLObjectType({
     genre: { type: GraphQLString },
     owner: {
       type: OwnerType,
-      resolve(parent, args) {
-        return owners.find(owner => owner.id === parent.ownerId)
+      resolve (parent, args) {
+        // return owners.find(owner => owner.id === parent.ownerId)
       }
     }
   })
@@ -63,8 +31,8 @@ const OwnerType = new GraphQLObjectType({
     lastName: { type: GraphQLString },
     pets: {
       type: new GraphQLList(PetType),
-      resolve(parent, args) {
-        return pets.filter(pet => pet.ownerId === parent.id)
+      resolve (parent, args) {
+        // return pets.filter(pet => pet.ownerId === parent.id)
       }
     }
   })
@@ -76,37 +44,72 @@ const RootQuery = new GraphQLObjectType({
     pet: {
       type: PetType,
       args: { id: { type: GraphQLID } },
-      resolve(parent, args) {
-        console.log(typeof(args.id))
-        return pets.find(pet => pet.id === args.id) 
+      resolve (parent, args) {
+        console.log(typeof (args.id))
+        // return pets.find(pet => pet.id === args.id)
       }
     },
     owner: {
       type: OwnerType,
       args: { id: { type: GraphQLID } },
-      resolve(parent, args) {
-        console.log(typeof(args.id))
-        return owners.find(owner => owner.id === args.id) 
+      resolve (parent, args) {
+        console.log(typeof (args.id))
+        // return owners.find(owner => owner.id === args.id)
       }
     },
     owners: {
       type: GraphQLList(OwnerType),
       args: {},
-      resolve(parent, args) {
-        console.log(typeof(args.id))
-        return owners 
+      resolve (parent, args) {
+        console.log(typeof (args.id))
+        // return owners
       }
     },
     pets: {
       type: GraphQLList(PetType),
       args: {},
-      resolve(parent, args) {
-        return pets
+      resolve (parent, args) {
+        // return pets
+      }
+    }
+  }
+})
+
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addPet: {
+      type: PetType,
+      args: {
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString }
+      },
+      resolve (parent, args) {
+        const pet = nano.use('pet')
+
+        pet.insert({ name: args.name, genre: args.genre })
+          .then(response => console.log(response))
+          .catch(err => console.error(err))
+      }
+    },
+    addOwner: {
+      type: OwnerType,
+      args: {
+        firstName: { type: GraphQLString },
+        lastName: { type: GraphQLString }
+      },
+      resolve (parent, args) {
+        const owner = nano.use('owner')
+
+        owner.insert({ firstName: args.firstName, lastName: args.lastName })
+          .then(response => console.log(response))
+          .catch(err => console.log(err))
       }
     }
   }
 })
 
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: Mutation
 })
--- server.js
+++ server.js
@@ -1,17 +1,11 @@
-const express = require("express")
-const { graphqlHTTP } = require("express-graphql") 
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema')
-
-const nano = require('nano')('http://admin:admin@db:5984')
-
-nano.db.get('graphql')
-  .then(data => console.log(data))
-  .catch(err => console.err(err))
 
 const app = express()
 // https://expressjs.com/en/guide/using-middleware.html
-app.use("/graphql", graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }))
-app.listen(5000, () => console.log('Running a GraphQL API server at http://localhost:5000/graphql'))
+app.listen(5000, () => console.log('Running a GraphQL API server'))
