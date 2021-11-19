e088a0c1c292a29cf661dd420fabd778155ec642
Bruno Mendoza <hola@brunomendoza.es>
2021-11-19 23:36:17 +0000

    Setting up CORS

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema')
-
 const app = express()
+
+app.use(cors())
 // https://expressjs.com/en/guide/using-middleware.html
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }))
-app.listen(5000, () => console.log('Running a GraphQL API server'))
+app.listen(5000, () => console.log('Running GraphQL API server'))
